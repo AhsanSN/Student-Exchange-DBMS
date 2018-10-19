603024abecb1330ab992645adce6287cbfbfec19
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-10-19 10:18:08 +0500

    head and menu seperated

---
+++ production/phpParts/leftMenu.php
@@ -0,0 +1,89 @@
+<div class="col-md-3 left_col">
+          <div class="left_col scroll-view">
+            <div class="navbar nav_title" style="border: 0;">
+              <a href="index.html" class="site_title"><i class="fa fa-paw"></i> <span>AHSAN OIL</span></a>
+            </div>
+
+            <div class="clearfix"></div>
+
+            <!-- menu profile quick info -->
+            <div class="profile clearfix">
+              <div class="profile_pic">
+                <img src="images/img.jpg" alt="..." class="img-circle profile_img">
+              </div>
+              <div class="profile_info">
+                <span>Welcome,</span>
+                <h2>Ahsan</h2>
+              </div>
+            </div>
+            <!-- /menu profile quick info -->
+
+            <br />
+
+            <!-- sidebar menu -->
+            <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
+              <div class="menu_section">
+                <h3>General</h3>
+                <ul class="nav side-menu">
+                  <li><a href="index.html"><i class="fa fa-home"></i> Home </span></a></li>
+                  <li><a><i class="fa fa-table"></i> View <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="form.html">Employees</a></li>
+                      <li><a href="form_advanced.html">Importers</a></li>
+                      <li><a href="form_advanced.html">Imports</a></li>
+                      <li><a href="form_validation.html">Exporters</a></li>
+                      <li><a href="form_validation.html">Exports</a></li>
+                      <li><a href="form_wizards.html">Transactions</a></li>
+                      <li><a href="form_buttons.html">Products</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-edit"></i> Insert <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="form.html">Employee</a></li>
+                      <li><a href="form_advanced.html">Importer</a></li>
+                      <li><a href="form_validation.html">Exporter</a></li>
+                      <li><a href="form_wizards.html">Transaction</a></li>
+                      <li><a href="form_buttons.html">Product</a></li>
+                    </ul>
+                  </li>                  
+                  <li><a><i class="fa fa-edit"></i> Edit <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="form.html">Employees</a></li>
+                      <li><a href="form_advanced.html">Importers</a></li>
+                      <li><a href="form_validation.html">Exporters</a></li>
+                      <li><a href="form_buttons.html">Products</a></li>
+                    </ul>
+                  </li>
+                  <li><a><i class="fa fa-edit"></i> Remove <span class="fa fa-chevron-down"></span></a>
+                    <ul class="nav child_menu">
+                      <li><a href="form.html">Employees</a></li>
+                      <li><a href="form_advanced.html">Importers</a></li>
+                      <li><a href="form_validation.html">Exporters</a></li>
+                      <li><a href="form_buttons.html">Products</a></li>
+                    </ul>
+                  </li>
+
+                </ul>
+              </div>
+              <br>
+              
+
+            </div>
+            <!-- /sidebar menu -->
+
+        
+            <!-- /menu footer buttons -->
+          </div>
+        </div>
+
+        
+        <!-- top navigation -->
+        <div class="top_nav">
+          <div class="nav_menu">
+            <nav>
+              <div class="nav toggle">
+                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+              </div>
+            </nav>
+          </div>
+        </div>
