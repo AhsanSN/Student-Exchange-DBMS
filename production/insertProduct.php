f25945199580af3864d067d3193de9e3accc5713
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-10-22 22:53:17 +0500

    keyingredient

--- production/insertProduct.php
+++ production/insertProduct.php
@@ -6,6 +6,7 @@ if(isset($_POST['title'])){
   $description = $_POST['description'];
   $productId = $_POST['productId'];
   $cost = $_POST['cost'];
+  $keyIngredient = $_POST['keyIngredient'];
 }
 
 ?>
@@ -57,6 +58,22 @@ if(isset($_POST['title'])){
                           <input name="productId" type="text" id="first-name" required="required" class="form-control col-md-7 col-xs-12">
                         </div>
                       </div>
+
+                     <div class="form-group">
+                          <label class="control-label col-md-3 col-sm-3 col-xs-12">Key Ingredient<span class="required"></span>
+                          </label>
+                          <div class="col-md-6 col-sm-6 col-xs-12">
+                          <div class="form-group">
+                        
+                          <select required="required" name="keyIngredient" class="select2_single form-control" tabindex="-1">
+                            <option value="Butane">Butane</option>
+                            <option value="Gasoline">Gasoline</option>
+                            <option value="Keroscene">Keroscene</option>
+                            <option value="Diesel">Diesel</option>                       
+                          </select>
+                      </div>
+                      </div>
+                    </div>                      
 
                       <div class="form-group">
                           <label class="control-label col-md-3 col-sm-3 col-xs-12">Cost<span class="required"></span>
