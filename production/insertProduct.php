c5cfc84776610ceb14c6038e1a2162d53d733f81
Syed Ahsan Ahmed <snahmed1998@gmail.com>
2018-10-22 22:50:29 +0500

    insertProduct added

---
+++ production/insertProduct.php
@@ -0,0 +1,91 @@
+<?php
+
+if(isset($_POST['title'])){
+
+  $title = $_POST['title'];
+  $description = $_POST['description'];
+  $productId = $_POST['productId'];
+  $cost = $_POST['cost'];
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <?php include_once("./phpParts/head.php")?>
+  <body class="nav-md">
+    <div class="container body">
+      <div class="main_container">
+        <?php include_once("./phpParts/leftMenu.php")?>
+        <!-- page content -->
+        <div class="right_col" role="main">
+          <div class="">
+            <div class="page-title">
+            </div>
+            <div class="clearfix"></div>
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Add Product</h2>
+                   
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                    <br />
+                    <form id="demo-form2" data-parsley-validate class="form-horizontal form-label-left" action="insertProduct.php" method="post">
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Title<span class="required"></span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input name="title" type="text" id="first-name" required="required" class="form-control col-md-7 col-xs-12">
+                        </div>
+                      </div>
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Description<span class="required"></span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <textarea id="message" required="required" class="form-control" name="description"></textarea>
+                        </div>
+                      </div>
+
+                      <div class="form-group">
+                        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="first-name">Product ID<span class="required"></span>
+                        </label>
+                        <div class="col-md-6 col-sm-6 col-xs-12">
+                          <input name="productId" type="text" id="first-name" required="required" class="form-control col-md-7 col-xs-12">
+                        </div>
+                      </div>
+
+                      <div class="form-group">
+                          <label class="control-label col-md-3 col-sm-3 col-xs-12">Cost<span class="required"></span>
+                          </label>
+                          <div class="col-md-6 col-sm-6 col-xs-12">
+                          <div class="form-group">
+                          <input required="required" type="number" name="cost" class="form-control" min="0">
+                      </div>
+                      </div>
+                    </div>
+
+                      <div class="ln_solid"></div>
+                      <div class="form-group">
+                        <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3">
+                          <button class="btn btn-primary" type="button">Cancel</button>
+                          <button type="submit" class="btn btn-success">Add</button>
+                        </div>
+                      </div>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- /footer content -->
+      </div>
+    </div>
+    <?php include_once("./phpParts/endScripts.php")?>
+  </body>
+
+</html>
